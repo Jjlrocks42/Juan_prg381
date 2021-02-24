16c5e64ad51ebb8eee9e5979951fda4b6f381437
JJlrocks42 <ludickjuan@gmail.com>
2021-02-24 10:24:40 +0200

    init

---
+++ prh381.java
@@ -0,0 +1,10 @@
+import javax.sound.sampled.Port;
+
+public class prh381 {
+
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+        System.out.println("First Java file on git");
+    }
+}
